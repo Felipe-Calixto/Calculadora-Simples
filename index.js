886208ef1e6e7ff9bfb4dfca174fa51f0df4c1a9
Felipe_Calixto03 <felipecalixtoribas@gmail.com>
2022-10-27 13:19:27 -0300

    commit

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+let number = document.querySelector("#number-result");
+
+//while(number.length < 10) {
+
 function insert(num) {
+    let numero = document.querySelector("#number-result").textContent;
+    console.log(numero);
+    if(numero.length < 10) {
     let number = document.querySelector("#number-result").innerHTML;
     document.querySelector("#number-result").innerHTML = number + num;
+    }
 }
 
 function apagar() {
@@ -12,3 +20,5 @@ function calcular() {
     
     document.querySelector("#number-result").innerHTML = eval(result);
 }
+
+//}
