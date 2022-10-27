ec89f85dbb99b016a837c699019c6727ea87c8e2
Felipe_Calixto03 <felipecalixtoribas@gmail.com>
2022-10-27 13:20:19 -0300

    commit

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 let number = document.querySelector("#number-result");
 
-//while(number.length < 10) {
-
 function insert(num) {
     let numero = document.querySelector("#number-result").textContent;
     console.log(numero);
@@ -21,4 +19,3 @@ function calcular() {
     document.querySelector("#number-result").innerHTML = eval(result);
 }
 
-//}
